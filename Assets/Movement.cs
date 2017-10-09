f29788828834ca3e03ba1cd5478aac8dd0024421
Patryk <patryk.nather@gmail.com>
2017-10-09 12:47:39 +0200

    Pacman is moving

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float movementSpeed = 0.001F;
+    public KeyCode keyUp = new KeyCode();
+    public KeyCode keyDown = new KeyCode();
+    public KeyCode keyRight = new KeyCode();
+    public KeyCode keyLeft = new KeyCode();
+
+    void Update()
+    {
+        if(Input.GetKey(keyUp))
+            transform.Translate(Vector3.up * movementSpeed * 0.005F);
+        else if (Input.GetKey(keyDown))
+            transform.Translate(Vector3.down * movementSpeed * 0.005F);
+        else if (Input.GetKey(keyRight))
+            transform.Translate(Vector3.right * movementSpeed * 0.005F);
+        else if (Input.GetKey(keyLeft))
+            transform.Translate(Vector3.left * movementSpeed * 0.005F);
+    }
 }
